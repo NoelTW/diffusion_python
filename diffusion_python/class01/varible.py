ecb88388aae6cafacac14667139d925485b7af43
Noel <as11111satw@gapp.nthu.edu.tw>
2022-04-23 02:15:43 +0800

    try update

---
+++ diffusion_python/class01/varible.py
@@ -0,0 +1,12 @@
+# number (int, float)
+
+# assign
+a,b = 1 +1 ,2*2 
+
+print(a,b) # comma seperte variables to print out
+# caculation
+
+your_poket = 100
+discount = .2
+drick_price = 25
+how_much_you_leave = your_poket - discount * drick_price
